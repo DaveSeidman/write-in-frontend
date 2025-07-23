9cb84e3e90ceb625b0bbd98d974fca98ceb7c90d
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2025-07-23 16:52:05 -0400

    swap in hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Quiz from './routes/quiz';
 import Question from './routes/question';
 import Results from './routes/results';
@@ -12,17 +12,16 @@ function App() {
       <Routes>
         <Route path="/" element={
           <div className="menu">
-            <a href="quiz">Quiz</a>
-            <a href="question">Question</a>
-            <a href="results">Results</a>
-            <a href="admin">Admin</a>
+            <a href="#/quiz">Quiz</a>
+            <a href="#/question">Question</a>
+            <a href="#/results">Results</a>
+            <a href="#/admin">Admin</a>
           </div>
         } />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/results" element={<Results />} />
         <Route path="/question" element={<Question />} />
         <Route path="/admin" element={<Admin />} />
-
       </Routes>
     </div>
   );
